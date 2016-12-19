dd6ae60f9e535bb3c703fcbcf7c8f3e715474e98
Greg <cryptomonger@hotmail.com>
2016-12-19 06:32:47 +0000

    nodemailer and mailgun are setup and working

--- public/js/angular.js
+++ public/js/angular.js
@@ -2,9 +2,26 @@
 var app = angular.module("app", ['ngRoute'])
 
 app.config(function($routeProvider, $locationProvider) {
-        $routeProvider.when('/', {
-            templateUrl: "ang-views/home.html"
-        }).otherwise({
-            redirectTo: '/'
-        });    }
+    $routeProvider.when('/', {
+        templateUrl: "/ang-views/home.html",
+        controller: "homeController"
+    }).when('/contact', {
+        templateUrl: "/ang-views/contact.html",
+        controller: "contactController"
+    }).otherwise({
+        redirectTo: '/'
+    });
+})
+
+
+app.controller('contactController', function($scope, $http) {
+    $scope.sub = function($scope) {
+    $http.post('/contact', $scope.formData).
+    success(function(data) {
+        console.log("posted success")
+        }).
+    error(function(data) {
+        console.error("posted failed")
+        })
+    }}
 )
--- server.js
+++ server.js
@@ -1,57 +1,62 @@
 'use strict';
 
 var express = require('express');
-var sass = require('node-sass');
-var sassMiddleware = require('node-sass-middleware');
 var http = require('http');
 var app = express();
+var app = require('express')();
+var bodyParser = require('body-parser');
+var multer = require('multer');
 
-app.use(sassMiddleware({
-        src: __dirname + '/public/sass',
-        dest: __dirname + '/public/stylesheets',
-        debug: true,
-        outputStyle: 'compressed',
-        prefix:  '/stylesheets'  // Where prefix is at <link rel="stylesheets" href="prefix/style.css"/>
-      }));
+var nodemailer = require('nodemailer');
+var mg = require('nodemailer-mailgun-transport');
 
-app.use(express.static("public"));
-
-app.get('/', (req, res)=>{res.sendFile(process.cwd() + '/public/layout.html')});
-
-// Command to run Server: nodemon ./server.js
-
-http.createServer(app).listen(process.env.PORT, process.env.IP);
-
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-    var err = new Error('Not Found');
-    err.status = 404;
-    next(err);
-});
-
-// error handlers
-
-// development error handler
-// will print stacktrace
-if (app.get('env') === 'development') {
-    app.use(function(err, req, res, next) {
-        res.status(err.status || 500);
-        res.render('error', {
-            message: err.message,
-            error: err
-        });
-    });
+// This is your API key that you retrieve from www.mailgun.com/cp (free up to 10K monthly emails)
+var auth = {
+  auth: {
+    api_key: 'key-eabf6a6b00d0805658ab06e9f10e30ec',
+    domain: 'mg.testophotography.com'
+  }
 }
 
-// production error handler
-// no stacktraces leaked to user
-app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-        message: err.message,
-        error: {}
-    });
+var nodemailerMailgun = nodemailer.createTransport(mg(auth));
+
+app.use(bodyParser.json()); // for parsing application/json
+app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
+
+app.post('/contact', function (req, res) {
+  console.log(req.body);
+  res.json(req.body);
 });
 
+// app.post('/contact', function(req, res) {
+//     nodemailerMailgun.sendMail({
+    
+//       from: "gtesto@testophotography.com",
+//       to: 'cryptomonger@hotmail.com', // An array if you have multiple recipients.
+//     //   cc:'second@domain.com',
+//     //   bcc:'secretagent@company.gov',
+//       subject: "subject",
+//       'h:Reply-To': 'gtesto@testophotography.com',
+//       //You can use "html:" to send HTML email content. It's magic!
+//       // html: '<b>Wow Big powerful letters</b>',
+//       // //You can use "text:" to send plain-text content. It's oldschool!
+//       text: req.data.name
+      
+//     }, function (err, info) {
+//       if (err) {
+//         console.log('Error: ' + err);
+//       }
+//       else {
+//         console.log('Response: ' + info);
+//       }
+//     });
+// });
 
+
+// Command to run Server: nodemon ./server.js
+http.createServer(app).listen(process.env.PORT, process.env.IP);
+
+app.use(express.static("public"));
+    
+app.get('/', (req, res)=>{res.sendFile(process.cwd() + '/public/layout.html')});
 module.exports = app;
